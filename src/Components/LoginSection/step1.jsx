9b00cf8096f948068a6b7b4bacc6d6a624b55c13
paravmahajan03 <paravmahajan03@gmail.com>
2024-10-09 20:53:32 +0530

    Title Updated

--- src/Components/LoginSection/step1.jsx
+++ src/Components/LoginSection/step1.jsx
@@ -8,10 +8,10 @@ export default function Step1({ data, handleUserData, handleStep, error }) {
   };
   return (
     <div className="flex justify-between sm:flex-col">
-      <div className="w-4/12 sm:w-10/12 mb-16">
+      <div className="w-4/12 xxl:w-5/12 sm:w-10/12 mb-16">
         <p className="text-2xl sm:text-2xl-sm">STEP 1</p>
         <h1 className="text-5xl sm:text-5xl-sm leading-5xl sm:leading-5xl-sm font-medium mb-4 sm:mb-2">
-          Enter your email address to continue
+          Enter your username to continue
         </h1>
         <p className="text-2xl sm:text-2xl-sm leading-2xl sm:leading-2xl-sm">
           Log in to your account. If you don’t have one, you will be prompted to
@@ -25,8 +25,8 @@ export default function Step1({ data, handleUserData, handleStep, error }) {
           type="text"
           id="username"
           required
-          placeholder="Email"
-          className="border rounded-[5px] w-full border-inputs py-[3.4%] px-[6.6%] sm:py-3 sm:px-6 placeholder-loginSection text-2xl sm:text-2xl-sm leading-2xl sm:leading-2xl-sm"
+          placeholder="Username"
+          className="border rounded-[5px] autofill:text-2xl w-full border-inputs py-[3.4%] px-[6.6%] sm:py-3 sm:px-6 placeholder-loginSection text-2xl sm:text-2xl-sm leading-2xl sm:leading-2xl-sm"
         />
         <p>{error}</p>
         <div className="flex justify-end gap-6 mt-[3.5%]">
